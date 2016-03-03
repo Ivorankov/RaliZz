a6e14d95fbaf2ef7048ae2e3a5881aaad601972b
Ivorankov <ivo_rankov@mail.bg>
2016-03-04 00:16:22 +0200

    some notifications added

--- ProjectTuber/app/controllers/project_controller.rb
+++ ProjectTuber/app/controllers/project_controller.rb
@@ -1,27 +1,36 @@
 class ProjectController < ApplicationController
   #require ProjectHelper
   skip_before_action :verify_authenticity_token
+  ERROR_MESSAGE = 'Invalid input {Title: min => 3 max => 30,
+                       Description: min => 10 max => 500,
+                       Link: max=>1000}'
+  SUCCESS_MESSAGE = 'You did it!'
+
   def index
     @projects = Project.paginate(:page => params[:page])
   end
 
   def add #TODO Define global constants for these
-    title = params[:title]
-    is_title_valid =  title.present?
-    description = params[:description]
-    is_description_valid = description.present?
-    link_to_source = params[:link_to_source]
-    is_link_valid = link_to_source.present?
+    case request.method_symbol
+      when :get
 
-    if is_title_valid && is_description_valid && is_link_valid
-      Project.create(:title => title,
-                     :description => description,
-                     :link_to_source => link_to_source)
-    else
-      flash[:alert] = 'Invalid input data'
+      when :post
+        title = params[:title]
+        description = params[:description]
+        link_to_source = params[:link_to_source]
+        ##is_link_valid = link_to_source.present?
 
+        if true
+          Project.create(:title => title,
+                         :description => description,
+                         :link_to_source => link_to_source)
+          flash[:success] = SUCCESS_MESSAGE
+        else
+          flash[:alert] = ERROR_MESSAGE
+        end
+      else
+        redirect_to project_path
     end
-
   end
 
   def update
